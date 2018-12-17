b475343011c5380a68165e24bfe82bb155f613ed
Vassileios Lamprakakis <v.e.lamprakakis@gmail.com>
2018-12-17 22:08:07 +0200

    Update humanresources.sql

--- databases/humanresources.sql
+++ databases/humanresources.sql
@@ -70,9 +70,6 @@ TABLESPACE pg_default;
 ALTER TABLE public.tmima
     OWNER to vassilis;
 	
---import data to tmima table
-INSERT INTO public.tmima (tmima_id, titlos) 
-SELECT DISTINCT tmima_id, tmima FROM public.giahumanresources
 
 -- table dieuthinsi
 CREATE TABLE public.dieuthinsi
@@ -89,11 +86,6 @@ TABLESPACE pg_default;
 ALTER TABLE public.dieuthinsi
     OWNER to vassilis;
 	
--- import data to dieuthinsi table
-INSERT INTO public.dieuthinsi (dieythinsi_id, titlos) 
-SELECT DISTINCT dieythinsi_id, dieythinsi FROM public.giahumanresources
-ON CONFLICT (dieythinsi_id)
-DO NOTHING;
 	
 -- table ypallilos
 CREATE TABLE public.ypallilos
@@ -138,9 +130,3 @@ CREATE INDEX fki_ypallilos_to_tmima
     (tmima_id COLLATE pg_catalog."default")
     TABLESPACE pg_default;	
 	
--- import data to ypallilos table
-INSERT INTO public.ypallilos (ypallilos_id, "katigoria symvasis", "katigoria ekpaid ", eidikotita, vathmos, fylo, dateofbirth, metaptyxiako,  didaktoriko, ethnikisxoli, dieythinsi_id, tmima_id) 
-SELECT DISTINCT ypallilos_id, katigoria_symvasis, katigoria_ekpaid, eidikotita, vathmos, fylo, dateofbirth, metaptyxiako, didaktoriko, ethnikisxoli, dieythinsi_id , tmima_id 
-FROM public.giahumanresources 
-ON CONFLICT (ypallilos_id)
-DO NOTHING;
