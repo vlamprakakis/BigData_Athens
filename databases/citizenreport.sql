3416eb1072af368c1aac5c870e93f7d76e773e86
Vassileios Lamprakakis <v.e.lamprakakis@gmail.com>
2018-12-17 22:03:11 +0200

    Update citizenreport.sql

--- databases/citizenreport.sql
+++ databases/citizenreport.sql
@@ -55,11 +55,6 @@ TABLESPACE pg_default;
 ALTER TABLE public.periohi
     OWNER to vassilis;
 	
--- import data to periohi table. From totalcitizendata table
-INSERT INTO public.periohi (periohi_id, periohiperigrafi, dimotikienotita) 
-SELECT DISTINCT periohi_id, periohiperigrafi, dimotikienotita FROM public.totalcitizendata 
-ON CONFLICT (periohi_id)
-DO NOTHING;
 
 -- ypiresia table 
 CREATE TABLE public.ypiresia
@@ -76,10 +71,6 @@ TABLESPACE pg_default;
 ALTER TABLE public.ypiresia
     OWNER to vassilis;
 	
--- import data to ypiresia table. From totalcitizendata table	
-INSERT INTO public.ypiresia (ypiresia_id, titlosypiresias) 
-SELECT DISTINCT ypiresia_id, titlosypiresias FROM public.totalcitizendata 
-
 -- aitima table 
 CREATE TABLE public.aitima
 (
@@ -120,9 +111,3 @@ CREATE INDEX fki_ypiresia_to_aitima
     (ypiresia_id COLLATE pg_catalog."default")
     TABLESPACE pg_default;
 	
--- Import data to aitima table. From totalcitizendata table
-INSERT INTO public.aitima (aitima_id, aitimadate, aitimaproodos, aitimakatigoria, aitimaperigrafi, periohistreet, periohisreetnum, ypiresia_id,  periohi_id) 
-SELECT DISTINCT aitima_id, aitimadate, aitimaproodos, aitimakatigoria, aitimaperigrafi, periohistreet, periohisreetnum, ypiresia_id,  periohi_id  
-FROM public.totalcitizendata 
-ON CONFLICT (aitima_id)
-DO NOTHING;
